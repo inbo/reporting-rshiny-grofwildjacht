1e4947005cc3293e1a0542c37d4978e1554dc139
pauwel.rummens@cloudar.be <pauwel.rummens@cloudar.be>
2017-06-20 08:41:25 +0200

    added grofwildapp

---
+++ scripts/reporting-rshiny-grofwildjacht.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Make a tar.gz from the R-package from the code
+cd /home/ubuntu/reporting-rshiny-grofwildjacht
+if [ -f reporting-grofwild.tar.gz ]; then
+    rm reporting-grofwild.tar.gz
+fi
+tar -zcvf reporting-grofwild.tar.gz reporting-grofwild
+# Build the docker image
+sudo docker build -t openanalytics/wildapp .

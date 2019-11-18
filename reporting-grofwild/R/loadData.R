ff43b788bbd9b1949123dc86cb3533aee9423ac3
mvarewyck <machteld.varewyck@openanalytics.eu>
2019-11-18 10:36:03 +0100

    update loading data

--- reporting-grofwild/R/loadData.R
+++ reporting-grofwild/R/loadData.R
@@ -281,11 +281,11 @@ loadRawData <- function(dataDir = system.file("extdata", package = "reportingGro
         
         
         # Re-define "Adult" as "Volwassen" for leeftijd + ordering levels
-#		rawData$leeftijdscategorie_MF[rawData$leeftijdscategorie_MF == "Adult"] <- "Volwassen"
+		rawData$leeftijdscategorie_MF[rawData$leeftijdscategorie_MF == "Adult"] <- "Volwassen"
         rawData$leeftijd_comp[rawData$leeftijd_comp == "Adult"] <- "Volwassen"
         
-#		rawData$Leeftijdscategorie_onderkaak[rawData$Leeftijdscategorie_onderkaak == "Adult"] <- "Volwassen"
-#		rawData$Leeftijdscategorie_onderkaak[rawData$Leeftijdscategorie_onderkaak %in% c("", "Onbekend")] <- "Niet ingezameld"
+		rawData$Leeftijdscategorie_onderkaak[rawData$Leeftijdscategorie_onderkaak == "Adult"] <- "Volwassen"
+		rawData$Leeftijdscategorie_onderkaak[rawData$Leeftijdscategorie_onderkaak %in% c("", "Onbekend")] <- "Niet ingezameld"
         
         # for Figure 13: combine age and gender: 'type' column 
         # (to do the matching with the openingstijden table)

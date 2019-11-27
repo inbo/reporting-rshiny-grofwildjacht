fc33964ae273727f8df6efa871f346101ea9b0b4
Sander Devisscher <sander.devisscher@inbo.be>
2019-11-27 11:58:44 +0100

    Revert "test scriptje"
    This reverts commit 66060248ac3782b9578e694d2de731645c6763a9.

--- scripts/filetje om huidige versie in branch te visualiseren.R
+++
@@ -1,9 +0,0 @@
-
-install.packages("ellipsis") 
-## => indien error => restart sessie
-
-devtools::install_github("inbo/reporting-rshiny-grofwildjacht@wildschade", subdir = "reporting-grofwild")
-devtools::build(pkg = "reporting-grofwild")
-devtools::install(pkg = "reporting-grofwild")
-library(reportingGrofwild)
-reportingGrofwild::runWildApp()

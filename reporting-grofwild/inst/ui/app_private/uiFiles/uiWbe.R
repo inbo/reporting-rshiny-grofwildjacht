d24aac084061c5a8bc783b7ed718b45bd8a9e96e
mvarewyck <machteld.varewyck@openanalytics.eu>
2022-01-31 09:19:00 +0100

    wbe afschot per jachtmethode

--- reporting-grofwild/inst/ui/app_private/uiFiles/uiWbe.R
+++ reporting-grofwild/inst/ui/app_private/uiFiles/uiWbe.R
@@ -16,25 +16,7 @@ tagList(
     ),
     
     tags$p(class = "lead", "Hier vindt u gedetailleerde info mbt tot uw wbe. Deze info is wachtwoord beschermd. U kan een login bekomen via HVV of de wbe secretaris."),
-  
-#    tags$p(tags$b(tags$u("Gebruiksinfo:")), tags$br(),
-#      tags$ul(
-#        tags$li("Bovenaan deze pagina krijgt u, in functie van de gekozen wildsoort, de regio-schaal en de gewenste jaren en periodes, een kaart met de geografische 
-#            spreiding van het afschot en een figuur met de evolutie van het afschot voor de gekozen periode."), 
-#        tags$li("Onder de verspreidingskaart kunnen verdere grafieken en tabellen worden opengeklikt, die telkens kunnen worden aangepast in functie van de informatie 
-#            die u wenst te visualiseren."), 
-#        tags$li("Onderstaande tabellen en figuren zijn gebaseerd op de beschikbare gegevens op ", format(max(as.Date(ecoData$afschot_datum), na.rm = T), 
-#            "%d/%m/%Y"), ". Een deel van de data van het voorbije kwartaal kunnen dus mogelijk nog niet opgenomen zijn in de dataset."),
-#        tags$li("De bron voor de kaarten, figuren en tabellen wordt gevormd door het",
-#          tags$a(href = "https://www.natuurenbos.be/e-loket", "E-loket van het Agentschap voor Natuur en Bos", target = "_blank"),
-#          ", gecombineerd met door het INBO uitgevoerde metingen op ingezamelde stalen (onderkaken en baarmoeders)."),
-#        tags$li("De data achter de figuren en tabellen kan je steeds downloaden als ruwe data."),
-#        tags$li("De figuren zelf kan je ook als .png downloaden."),
-#        tags$li("Indien u fouten zou ontdekken of merkt dat data ontbreken gelieve dit dan te melden via een email naar",
-#          tags$a(href="mailto:faunabeheer@inbo.be?SUBJECT=Grofwildjacht web applicatie", target="_blank", "faunabeheer@inbo.be"), ".")
-#      )
-#    )
-  
+   
   ),
   
   # Select species
@@ -53,7 +35,8 @@ tagList(
           HTML("<div class='fotoTitel'>Wild zwijn</div><div id='wildZwijnFoto'></div>"),
           HTML("<div class='fotoTitel'>Ree</div><div id='reeFoto'></div>"),
           HTML("<div class='fotoTitel'>Damhert</div><div id='damhertFoto'></div>"),
-          HTML("<div class='fotoTitel'>Edelhert</div><div id='edelhertFoto'></div>"))
+          HTML("<div class='fotoTitel'>Edelhert</div><div id='edelhertFoto'></div>")),
+        selected = "Ree"
       )
     )
   ),
@@ -167,7 +150,30 @@ tagList(
         
         ) 
         
-      })
+      }),
+      
+      
+      conditionalPanel("input.wbe_species == 'Ree'", {
+        
+      tagList(
+        actionLink(inputId = "wbe_linkPlot3", label =
+            h3("FIGUUR: Afschot per jachtmethode")),
+        conditionalPanel("input.wbe_linkPlot3 % 2 == 1",
+          fixedRow(
+            
+            column(4,
+              optionsModuleUI(id = "wbe_plot3", showTime = TRUE, 
+                regionLevels = c(1:2,4), exportData = TRUE),
+              tags$p("Aandeel van afschot per jachtmethode over de jaren heen.")
+            ),
+            column(8, plotModuleUI(id = "wbe_plot3"))
+          
+          ),
+          tags$hr()
+        )
+      )
+      
+    })
   
   )
 

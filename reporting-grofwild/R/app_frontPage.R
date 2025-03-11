1c618e275e43de89f8a1441151f554805840f030
mvarewyck <machteld.varewyck@openanalytics.eu>
2025-03-11 12:51:02 +0100

    fix #501 - restyle home page: full width image

--- reporting-grofwild/R/app_frontPage.R
+++ reporting-grofwild/R/app_frontPage.R
@@ -12,38 +12,31 @@ frontUI <- function(speciesList){
       package = "reportingGrofwild"
   )
   
-  fluidPage(
-    fluidRow(
-      slickR::slickR(
+  bootstrapPage(
+    slickR::slickR(
         obj = img, slideId = "carousel", 
         slideType = 'img',
-        width = "100%", padding = 0,
-        height = "95vh;"
+        width = "100vw", padding = 0
       ) + 
-      slickR::settings(dots = TRUE, arrows = FALSE, autoplay = TRUE)
-    ),
-    fluidRow(style = "margin-top: -25em",
-      column(
-        width = 6, offset = 3, align = "center",
+      slickR::settings(dots = TRUE, arrows = FALSE, autoplay = TRUE),
+    fluidRow(style = "margin-top: -50vh",
+      column(width = 6, offset = 3, align = "center",
         tags$span(
-          style = "font-weight: bold;text-align: center;color: white;font-size: 2.1em",
+          style = "font-weight: bold;text-align: center;color: white;font-size: 2.1em;",
           "Welkom op de faunabeheer pagina van het",
           br(),
           "Instituut voor Natuur- en Bosonderzoek (INBO)"
-        )
-      ), 
-    ),
-    fluidRow(style = "margin-top: 20em", 
-      column(
-        width = 6, offset = 3, align = "center",
-        selectInput(
-          inputId = "specie", 
-          label = tags$span(
-            style = "color: white; font-size: 1.5em", 
-            "Selecteer een diersoort:"
-          ),
-          choices = c("", speciesList),
-          width = "100%"
+        ),
+        tags$div(style = "margin-top: 10vh;",
+          selectInput(
+            inputId = "specie", 
+            label = tags$span(
+              style = "color: white; font-size: 1.5em", 
+              "Selecteer een diersoort:"
+            ),
+            choices = c("", speciesList),
+            width = "100%"
+          )
         )
       )
     )

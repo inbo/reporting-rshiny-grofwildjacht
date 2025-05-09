81cd71bda3db2a4b16937725c2d234416624a1d7
mvarewyck <machteld.varewyck@openanalytics.eu>
2025-05-09 17:02:11 +0200

    add box with partners on front page

--- reporting-grofwild/R/app_frontPage.R
+++ reporting-grofwild/R/app_frontPage.R
@@ -35,7 +35,18 @@ frontUI <- function(speciesList){
             choices = c("", speciesList),
             width = "100%"
           )
-        )
+        ),
+        
+        tags$div(class = "frontbox", 
+          HTML(paste("Deze pagina is tot stand gekomen in samenwerking met het", 
+            "<a href='https://www.natuurenbos.be' target='_blank'>Agentschap voor Natuur en Bos</a>,", 
+            "en bundelt gegevens van het e-loket fauna en flora",
+            "<a href='https://www.natuurenbos.be/e-loket' target='_blank'>(ANB)</a>,",
+            "het INBO, Natuurpunt",
+            "<a href='https://waarnemingen.be' target='_blank'>(waarnemingen.be)</a>,",
+            "en Wilder",
+            "<a href='https://hvv.be' target='_blank'>(Hubertus Vereniging Vlaanderen)</a>")
+        ))
       )
     )
   )

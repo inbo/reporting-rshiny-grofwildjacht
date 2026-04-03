35ae17e03aba53a756acded68d93306d12b91027
LouiseDeconinck <louise.deconinck@openanalytics.eu>
2026-04-03 16:50:27 +0200

    Add filter on wolf deaths (#660)
    * Add extra filter in countDeathWolves, to only keep known causes of death and ensure only dead wolves are kept

--- reporting-grofwild/R/countDeathWolves.R
+++ reporting-grofwild/R/countDeathWolves.R
@@ -25,10 +25,16 @@ countDeathWolves <- function(data, jaartallen = NULL,
   regio = "",	width = NULL, height = NULL) {
 	
     summarizeBy <- match.arg(summarizeBy)
-    
-  # Remove some categories
-  data <- data[data[[groupVariable]] != "AANVULLEN" & data$Levend == 0, ]
   
+  # Remove some categories
+  # TODO: this might be a superfluous filter
+  data <- data[data[[groupVariable]] != "AANVULLEN" & data$Levend == 0, ]
+
+  # Additional filtering
+  data <- data |>
+    dplyr::filter(.data$Lot != "Onbekend") |>
+    dplyr::filter(.data$Levend == 0)
+
 	if (is.null(jaartallen))
 		jaartallen <- unique(data$year)
 	
